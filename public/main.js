d1fd38d366c06e90cd1370cd2323fa0324ad1076
Ulises-Saucedo <ulisesaucedo.se04@gmail.com>
2023-08-28 08:19:44 -0300

    Nuevo header

---
+++ public/main.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", () =>{
+    const schoolLink = document.querySelector(".school a")
+    const schoolItem = document.querySelector(".school")
+    const specialtiesLink = document.querySelector(".specialties a")
+    const specialtiesItem = document.querySelector(".specialties")
+    const toggleIcon = document.querySelectorAll(".toggle-icon")
+
+    schoolLink.addEventListener("click", event =>{
+        event.preventDefault()
+        schoolItem.classList.toggle("active")
+    })
+    specialtiesLink.addEventListener("click", event =>{
+        event.preventDefault()
+        specialtiesItem.classList.toggle("active")
+    })
+    toggleIcon.forEach( toggleIcon =>{
+        const icon = toggleIcon.querySelector("i");
+        toggleIcon.addEventListener("click", event =>{
+            event.preventDefault();
+            icon.classList.toggle("bi-caret-down")
+            icon.classList.toggle("bi-caret-up")
+        });
+    });
+
+})
