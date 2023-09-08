cf95e504fcaada2cb74cb2dadbd9334084d01ab2
Ulises-Saucedo <ulisesaucedo.se04@gmail.com>
2023-09-08 10:53:56 -0300

    fix

--- public/main.js
+++ public/main.js
@@ -1,28 +1,3 @@
-document.addEventListener("DOMContentLoaded", () =>{
-    const schoolLink = document.querySelector(".school a")
-    const schoolItem = document.querySelector(".school")
-    const specialtiesLink = document.querySelector(".specialties a")
-    const specialtiesItem = document.querySelector(".specialties")
-    const toggleIcon = document.querySelectorAll(".toggle-icon")
-
-    schoolLink.addEventListener("click", event =>{
-        event.preventDefault()
-        schoolItem.classList.toggle("active")
-    })
-    specialtiesLink.addEventListener("click", event =>{
-        event.preventDefault()
-        specialtiesItem.classList.toggle("active")
-    })
-    toggleIcon.forEach( toggleIcon =>{
-        const icon = toggleIcon.querySelector("i");
-        toggleIcon.addEventListener("click", event =>{
-            event.preventDefault();
-            icon.classList.toggle("bi-caret-down")
-            icon.classList.toggle("bi-caret-up")
-        });
-    });
-})
-
 VanillaTilt.init(document.querySelectorAll(".box"), {
     max: 10,
     speed: 400
